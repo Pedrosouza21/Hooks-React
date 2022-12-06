b222d19ef7746c9041abf9e0b5b390a3208f7b48
Pedrosouza21 <pedrohenriqueoliveiradesouza9@gmail.com>
2022-12-06 11:15:15 -0300

    Custom Hooks #01

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,8 @@
 import {numberReducer} from './number'
 import { userReducer } from './user'
 
-export function reducer(state, action) {
+export function reducer(state, action) 
+{
    let newState = numberReducer(state, action)
    return userReducer(newState, action) 
 }
